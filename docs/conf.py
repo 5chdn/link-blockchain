d0eaab0337d7ed36f1af0ed47b4dd49a92653132
Jonathan Brown <jbrown@bluedroplet.com>
2016-12-13 17:37:41 +0700

    Update conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -49,6 +49,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'Link Blockchain'
+author = u'Jonathan Brown'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
